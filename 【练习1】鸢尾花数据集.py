e82bebf9b9f74fd483b022e822425f5fb0ca7415
Nephele <71633656+orange-Nan@users.noreply.github.com>
2022-02-24 13:48:04 +0800

    Update 【练习1】鸢尾花数据集.py

--- 【练习1】鸢尾花数据集.py
+++ 【练习1】鸢尾花数据集.py
@@ -18,39 +18,30 @@ url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
 dataset = pandas.read_csv(url, names=names)
 
-
+#数据集基本信息
 #print(dataset.shape)   #shape数据集维度
 #print(dataset.head(20))   #数据集前20行
 #print(dataset.describe())   #descriptions统计摘要（数量、平均值、最大值、最小值等）
 #print(dataset.groupby('class').size())  #class distribution每一个类别（每一种鸢尾花）行的数量
 
 #数据可视化
-#为每一个变量创建箱线图
-#dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
-#plt.show()
-#为每一个变量创建直方图（其中有2个变量呈高斯分布（正态分布））
-#dataset.hist()
-#plt.show()
-#全部属性对的散点图，这有助于我们看出输入变量之间的结构化关系
-#注意一些属性对呈对角线分布，这显示了它们之间有高度关联性以及可预测的关系#
-#scatter_matrix(dataset)
-#plt.show()
+#dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)  #为每一个变量创建箱线图
+#dataset.hist() #为每一个变量创建直方图（其中有2个变量呈高斯分布（正态分布））
+#scatter_matrix(dataset)  #全部属性对的散点图，注意一些属性对呈对角线分布，这显示了它们之间有高度关联性以及可预测的关系
+#plt.show()  #显示图片（如果想同时呈现以上三种图片，可以创建子图，在子图中绘制）
 
 #评估算法
 #1.创建验证集（80%训练，20%验证）
 #得到的 X_train 和 Y_train 里的训练数据用于准备模型
-#得到的 X_validation 和 Y_validation 集我们后面会用到
 array = dataset.values
 X = array[:,0:4]
 Y = array[:,4]
-
 validation_size = 0.20
 seed = 7
 X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
 #2.使用十折交叉验证法测试模型的准确度
 scoring = 'accuracy'
 #3.搭建和评估模型
-# Spot Check Algorithms
 models = []
 models.append(('LR', LogisticRegression()))
 models.append(('LDA', LinearDiscriminantAnalysis()))
