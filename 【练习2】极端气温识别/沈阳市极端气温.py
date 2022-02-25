3af72e35e144c6017d7785b847b77014e16f858e
Nephele <71633656+orange-Nan@users.noreply.github.com>
2022-02-25 12:34:31 +0800

    Update 沈阳市极端气温.py

--- 【练习2】极端气温识别/沈阳市极端气温.py
+++ 【练习2】极端气温识别/沈阳市极端气温.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[10]:
-
-
 import numpy as np
 import pandas as pd
 from pandas import DataFrame
@@ -11,21 +5,21 @@ import matplotlib.pyplot as plt
 import datetime
 from sklearn.cluster import KMeans
 
-
+#导入数据
 f = r'C:\Users\LULU\Desktop\biyelunwen\shuju\Shenyang_meteorological_data.csv'
 sheet = pd.read_csv(f,header=0)
 data = DataFrame(sheet)
 
-#去除异常值-999
+#去除异常值-9999
 data = data.replace(-9999,np.nan)
 data = data.dropna()
 
-data['date'] = pd.to_datetime(data['date'],format = '%Y/%m/%d %H:00')
-data['T'] = data['T'].map(lambda x:x/10)
+data['date'] = pd.to_datetime(data['date'],format = '%Y/%m/%d %H:00')   #转换为datetime格式
+data['T'] = data['T'].map(lambda x:x/10)   #原始数据是乘了十倍的
 
-print(len(data['T']))  #15839
+print(len(data['T']))  #一共15839行数据
 
-
+#转换为符合Kmeans要求的数组
 y1 = list(data['T'])
 y2 = np.array(y1)
 y2 = y2.reshape(-1,1)
@@ -36,7 +30,7 @@ y = KMeans(n_clusters=50).fit_predict(y2)
 
 fig = plt.figure(figsize=(10,10),dpi=200)
 ax1=fig.add_subplot(111)
-ax1.scatter(data['date'],data['T'],c=y)
+ax1.scatter(data['date'],data['T'],c=y)  #使用date和T画散点图，Kmeans分成的不同的类别用不同颜色标出
 ax1.set_xlabel('Time',fontsize=15)
 ax1.set_ylabel('Temperature(°C)',fontsize=15)
 plt.suptitle('Distribution of Temperature in Shenyang',fontsize=20,y=0.92)
@@ -44,7 +38,6 @@ plt.savefig('C:/Users/LULU/Desktop/Kmean.jpg')
 plt.show()
 
 
-# In[ ]:
 
 
 
